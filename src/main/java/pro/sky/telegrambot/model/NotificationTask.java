0afd5bdcd01fc7115dc0a95f13d8efcfa41f54c2
AlexanderRyaz <110193564+AlexanderRyaz@users.noreply.github.com>
2023-03-10 22:02:06 +0300

    сделано 2.1-2.4

--- src/main/java/pro/sky/telegrambot/model/NotificationTask.java
+++ src/main/java/pro/sky/telegrambot/model/NotificationTask.java
@@ -1,10 +1,15 @@
 package pro.sky.telegrambot.model;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "notification_task")
+@Data
 public class NotificationTask {
     @Id
     private Long id;
