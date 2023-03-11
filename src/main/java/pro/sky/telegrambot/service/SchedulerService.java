7d0ade69caf911271489a317111c6eb0e7d8eb94
AlexanderRyaz <110193564+AlexanderRyaz@users.noreply.github.com>
2023-03-11 22:41:31 +0300

    сделано 2.1-2.5

--- src/main/java/pro/sky/telegrambot/service/SchedulerService.java
+++ src/main/java/pro/sky/telegrambot/service/SchedulerService.java
@@ -1,5 +1,7 @@
 package pro.sky.telegrambot.service;
 
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.response.SendResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -15,16 +17,21 @@ import java.util.List;
 
 public class SchedulerService {
     private NotificationTaskRepository notificationTaskRepository;
+    private TelegramBot telegramBot;
 
     @Autowired
-    public SchedulerService(NotificationTaskRepository notificationTaskRepository) {
+    public SchedulerService(NotificationTaskRepository notificationTaskRepository, TelegramBot telegramBot) {
         this.notificationTaskRepository = notificationTaskRepository;
+        this.telegramBot = telegramBot;
     }
 
     @Scheduled(cron = "0 0/1 * * * *")
     public void scheduler() {
         LocalDateTime currentTime = LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES);
         List<NotificationTask> allBySentAt = notificationTaskRepository.findAllBySentAt(currentTime);
-        allBySentAt.forEach(notificationTask -> new SendMessage(notificationTask.getChatId(), notificationTask.getText()));
+        allBySentAt.forEach(notificationTask -> {
+            SendMessage message = new SendMessage(notificationTask.getChatId(), notificationTask.getText());
+            telegramBot.execute(message);
+        });
     }
 }
